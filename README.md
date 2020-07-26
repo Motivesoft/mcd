e658e4f526e49eff1fdd54cb44f62b513153b7bc
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2020-07-26 20:48:00 +0200

    Initial commit with basic structure, and help text

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mcd
-Command line executable to make and directory and change into it as a single command - MD and CD.  
+Command line executable to make and directory and change into it as a single command - a combo `md` and `cd`.
+
+# What and why?
+For me, when I make a directory in a command prompt window, I usually also want to change into it.
+
+Fed up of typing `md x\y\z` and then having to also type `cd x\y\z` straight afterwards, I decided a simple utility should let me do this as a single instruction.
+
+The general idea is that `mcd` should work just like the inbuild command tool `cd`, but that if the requested directory doesn't exist, it does a bit of `md` magic first.
+
+Yes, it would be easy enough to do this in a batch or command file, or by some other method, but it seemed like a nice easy gadget that someone might get some use from.
+
+# Notes
+1. This will be a basic C++ application written using Visual Studio
+1. This will be written for wide character sets (`wchar_t`, rather than `char`), but should be fairly easy to convert back to a non-wide version
+1. Syntactically, the utility will work like `cd` but of course you change that that by copying this code and making you own version
+1. Built executables will be provided as a GitHub Release
+1. The best usage is to put the appropriate executable into a utilities folder that is on your command prompt path so that you can run it just as simply as you can use the built in `cd` and `md` commands.
---
+++ mcd/mcd.cpp
@@ -0,0 +1,107 @@
+// mcd.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+void printCurrentWorkingDirectory();
+bool isHelp( const wchar_t* arg );
+void printHelp( const wchar_t* name );
+
+int wmain( int argc, const wchar_t* argv[] )
+{
+    if ( argc == 1 )
+    {
+       printCurrentWorkingDirectory();
+    }
+    else
+    {
+       std::wstring path;
+       bool showHelp = false;
+       for ( int i = 1; i < argc && !showHelp; i++ )
+       {
+          showHelp = isHelp( argv[ i ] );
+          if ( !showHelp )
+          {
+             if ( path.length() > 0 )
+             {
+                path = path.append( L" " );
+             }
+             path.append( argv[ i ] );
+          }
+       }
+
+       if ( showHelp )
+       {
+          wchar_t appName[ _MAX_FNAME ];
+          _wsplitpath_s( argv[ 0 ], NULL, 0, NULL, 0, appName, _MAX_FNAME, NULL, 0 );
+          _wcsupr_s( appName, _MAX_FNAME );
+
+          printHelp( appName );
+       }
+       else
+       {
+          std::wcout << "\"" << path << "\"" << std::endl;
+       }
+    }
+}
+
+bool isHelp( const wchar_t* arg )
+{
+   return arg != NULL && wcscmp( arg, L"/?" ) == 0;
+}
+
+void printHelp( const wchar_t* name )
+{
+   std::wcout << "Displays the name of or changes the current directory. If the directory doesn't exist, it is created." << std::endl;
+   std::wcout << "" << std::endl;
+   std::wcout << name << " [/D] [drive:][path]" << std::endl;
+   std::wcout << name << " [..]" << std::endl;
+   std::wcout << "" << std::endl;
+   std::wcout << "  ..   Specifies that you want to change to the parent directory." << std::endl;
+   std::wcout << "" << std::endl;
+   std::wcout << "Type " << name << " drive: to display the current directory in the specified drive." << std::endl;
+   std::wcout << "Type " << name << " without parameters to display the current drive and directory." << std::endl;
+   std::wcout << "" << std::endl;
+   std::wcout << "Use the /D switch to change current drive in addition to changing current" << std::endl;
+   std::wcout << "directory for a drive." << std::endl;
+   std::wcout << "" << std::endl;
+   std::wcout << name << " does not treat spaces as delimiters, so it is possible to" << std::endl;
+   std::wcout << "create and change into a subdirectory name that contains a space without surrounding" << std::endl;
+   std::wcout << "the name with quotes.  For example:" << std::endl;
+   std::wcout << "" << std::endl;
+   std::wcout << "    " << name << " \\winnt\\profiles\\username\\programs\\start menu" << std::endl;
+   std::wcout << "" << std::endl;
+   std::wcout << "is the same as:" << std::endl;
+   std::wcout << "" << std::endl;
+   std::wcout << "    " << name << " \"\\winnt\\profiles\\username\\programs\\start menu\"" << std::endl;
+   std::wcout << "" << std::endl;
+   std::wcout << "Multiple adjacent spaces will be condensed down to one, which is not precisely" << std::endl;
+   std::wcout << "the same behaviour as CD with extensions enabled, but close enough for most uses. For" << std::endl;
+   std::wcout << "multiple adjecent spaces, surround the path with quotes.  For example:" << std::endl;
+   std::wcout << "" << std::endl;
+   std::wcout << "    " << name << " \"\\temp\\this     path  has    many       spaces\"" << std::endl;
+   /*
+The current directory string is converted to use the same case as
+the on disk names.  So CD C:\TEMP would actually set the current
+directory to C:\Temp if that is the case on disk.
+
+which is what you would have to type if extensions were disabled.
+   */
+}
+
+void printCurrentWorkingDirectory()
+{
+   wchar_t* buffer = _wgetcwd( NULL, _MAX_PATH );
+   __try
+   {
+      if ( buffer )
+      {
+         std::wcout << buffer << std::endl;
+      }
+   }
+   __finally
+   {
+      free( buffer );
+   }
+
+}
